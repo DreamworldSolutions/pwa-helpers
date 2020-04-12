8efaf87203c8632967b0dedb4027ea2a59901170
Nirmal Baldaniya <nirmalb@codetailor.in>
2020-04-12 18:16:20 +0530

    feat: connect mixin
    - catch exception & log error when stateChanged fails from a view. so, other views can get stateChange notification.
    - Trigger stateChanged ONLY when the actual state is changed. e.g. When just action is dispatched but reducer doesn't do anything on that action then actual state isn't changed

---
+++ connect-mixin.js
@@ -0,0 +1,52 @@
+
+/**
+  This is a JavaScript mixin that you can use to connect a Custom Element base
+  class to a Redux store. The `stateChanged(state)` method will be called when
+  the state is updated.
+
+  Example:
+
+      import { connect } from '@dreamworld/pwa-helpers/connect-mixin.js';
+
+      class MyElement extends connect(store)(HTMLElement) {
+        stateChanged(state) {
+          this.textContent = state.data.count.toString();
+        }
+      }
+*/
+export const connect = (store) => (baseElement) => class extends baseElement {
+  connectedCallback() {
+      if (super.connectedCallback) {
+          super.connectedCallback();
+      }
+      this._storeUnsubscribe = store.subscribe(() => this.__stateChanged(store.getState()));
+      this.__stateChanged(store.getState());
+  }
+  disconnectedCallback() {
+      this._storeUnsubscribe();
+      if (super.disconnectedCallback) {
+          super.disconnectedCallback();
+      }
+  }
+
+  /**
+   * Log console error of `stateChanged`.
+   * Trigger `stateChanged` only when actual state is changed.
+   */
+  __stateChanged(state) {
+    try {
+      if (this.__previousState === state) {
+        return;
+      }
+      this.stateChanged(state);
+      this.__previousState = state;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  /**
+   * The `stateChanged(state)` method will be called when the state is updated.
+   */
+  stateChanged(_state) { }
+};
+//# sourceMappingURL=connect-mixin.js.map
