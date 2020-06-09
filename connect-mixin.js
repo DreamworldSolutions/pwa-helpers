fb168c8756acb57f53756bb07e21277bf68051e3
Nirmal Baldaniya <nirmalb@codetailor.in>
2020-06-09 20:07:29 +0530

    feat(connect-mixin): add active property getter/setter in connect mixin.

--- connect-mixin.js
+++ connect-mixin.js
@@ -1,4 +1,4 @@
-
+import debounce from 'lodash-es/debounce';
 /**
   This is a JavaScript mixin that you can use to connect a Custom Element base
   class to a Redux store. The `stateChanged(state)` method will be called when
@@ -16,17 +16,39 @@
 */
 export const connect = (store) => (baseElement) => class extends baseElement {
   connectedCallback() {
-      if (super.connectedCallback) {
-          super.connectedCallback();
-      }
-      this._storeUnsubscribe = store.subscribe(() => this.__stateChanged(store.getState()));
-      this.__stateChanged(store.getState());
+    if (super.connectedCallback) {
+        super.connectedCallback();
+    }
+    this._debounceStateChanged = debounce(this.__stateChanged, 50);
+    this._storeUnsubscribe = store.subscribe(() => this._debounceStateChanged(store.getState()));
+    this._debounceStateChanged(store.getState());
   }
   disconnectedCallback() {
       this._storeUnsubscribe();
       if (super.disconnectedCallback) {
           super.disconnectedCallback();
       }
+  }
+
+    /**
+   * Setter of active property.
+   */
+  set active(val) {
+    let oldValue = this._active;
+    if(val === oldValue) {
+      return;
+    }
+
+    this._active = val;
+    this.requestUpdate('active', oldValue);
+    this._debounceStateChanged && this._debounceStateChanged(store.getState());
+  }
+
+  /**
+   * Getter of active property.
+   */
+  get active() {
+    return this._active;
   }
 
   /**
@@ -38,6 +60,7 @@ export const connect = (store) => (baseElement) => class extends baseElement {
       if (this.__previousState === state) {
         return;
       }
+      console.log('state changed', this.tagName);
       this.stateChanged(state);
       this.__previousState = state;
     } catch (err) {
