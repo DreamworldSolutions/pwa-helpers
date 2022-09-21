3e0a11b2fd232d75f8cd01f2881d83bcd0b5b9b1
devangdreamworld <devang@hisab.co>
2022-09-21 15:13:56 +0530

    fix: code review reworks

--- utils.js
+++ utils.js
@@ -1,22 +1,25 @@
+/**
+ * Identifies whether a WebComponent with a name is already registered? Check is performed in both: customElements and Polymer.telemetry registries.
+ * @param {String} elName - WebComponent name
+ * @returns Boolean
+ */
+
 export const isElementAlreadyRegistered = (elName) => {
   var registeredElement;
+  if (window && window.customElements) {
+    registeredElement = window.customElements.get(elName);
+    if (registeredElement) {
+      return true;
+    }
+  }
+
   if (window.Polymer && Polymer.telemetry) {
     registeredElement = Polymer.telemetry.registrations.filter((el) => el.is == elName);
 
     if (registeredElement) {
-      console.warn(elName, "is already registered as a custom polymer element.");
+      return true;
     }
-
-    return !!registeredElement;
   }
 
-  if (window && window.customElements) {
-    registeredElement = window.customElements.get(elName);
-
-    if (registeredElement) {
-      console.warn(elName, "is already registered as a custom lit element.");
-    }
-
-    return !!registeredElement;
-  }
+  return false;
 };
