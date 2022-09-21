d46be1f05483b4fa42dbb243536e8744ed4b4a07
devangdreamworld <devang@hisab.co>
2022-09-21 12:02:01 +0530

    fix: format code

--- utils.js
+++ utils.js
@@ -1,22 +1,22 @@
 export const isElementAlreadyRegistered = (elName) => {
   var registeredElement;
-  if(window.Polymer && Polymer.telemetry ) {
+  if (window.Polymer && Polymer.telemetry) {
     registeredElement = Polymer.telemetry.registrations.filter((el) => el.is == elName);
 
-    if(registeredElement) {
+    if (registeredElement) {
       console.warn(elName, "is already registered as a custom polymer element.");
     }
 
     return !!registeredElement;
   }
 
-  if(window && window.customElements ) {
+  if (window && window.customElements) {
     registeredElement = window.customElements.get(elName);
 
-    if(registeredElement) {
+    if (registeredElement) {
       console.warn(elName, "is already registered as a custom lit element.");
     }
 
     return !!registeredElement;
   }
-}
+};
