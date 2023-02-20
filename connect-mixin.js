c220a090e652575547efeebbd574a76bb754cf9e
“Nirmal <nirmalb@dreamworld.dev>
2023-02-20 16:57:33 +0530

    feat(connect-mixin): Make connect-mixin SSR compatible.

--- connect-mixin.js
+++ connect-mixin.js
@@ -15,62 +15,76 @@
         }
       }
 */
-export const connect = (store) => (baseElement) => class extends baseElement {
-  connectedCallback() {
-    if (super.connectedCallback) {
-        super.connectedCallback();
+export const connect = (store) => (baseElement) =>
+  class extends baseElement {
+    willUpdate(changedProps) {
+      if (super.willUpdate) {
+        super.willUpdate(changedProps);
+      }
+
+      if (this.request && this.request.store) {
+        this.__stateChanged(this.request.store.getState());
+      }
     }
-    this._storeUnsubscribe = store.subscribe(() => this.__stateChanged(store.getState()));
-    this.__stateChanged(store.getState());
-  }
-  disconnectedCallback() {
+
+    connectedCallback() {
+      if (super.connectedCallback) {
+        super.connectedCallback();
+      }
+      this._storeUnsubscribe = store.subscribe(() =>
+        this.__stateChanged(store.getState())
+      );
+      this.__stateChanged(store.getState());
+    }
+
+    disconnectedCallback() {
       this._storeUnsubscribe && this._storeUnsubscribe();
       if (super.disconnectedCallback) {
-          super.disconnectedCallback();
+        super.disconnectedCallback();
       }
-  }
+    }
 
     /**
-   * Setter of active property.
-   */
-  set active(val) {
-    let oldValue = this._active;
-    if(val === oldValue) {
-      return;
-    }
-
-    this._active = val;
-    this.requestUpdate('active', oldValue);
-    this.isConnected && this.__stateChanged(store.getState());
-  }
-
-  /**
-   * Getter of active property.
-   */
-  get active() {
-    return this._active;
-  }
-
-  /**
-   * Log console error of `stateChanged`.
-   * Trigger `stateChanged` only when actual state is changed & active property is `true`
-   * When `active` property is not defined it will not prevent `stateChanged` call unless state is not changed.
-   */
-  __stateChanged(state) {
-    try {
-      if (this.__previousState === state || (this.active === false)) {
+     * Setter of active property.
+     */
+    set active(val) {
+      let oldValue = this._active;
+      if (val === oldValue) {
         return;
       }
-      
-      this.stateChanged(state);
-      this.__previousState = state;
-    } catch (err) {
-      console.error(err);
+
+      this._active = val;
+      this.requestUpdate("active", oldValue);
+      this.isConnected && this.__stateChanged(store.getState());
     }
-  }
-  /**
-   * The `stateChanged(state)` method will be called when the state is updated.
-   */
-  stateChanged(_state) { }
-};
+
+    /**
+     * Getter of active property.
+     */
+    get active() {
+      return this._active;
+    }
+
+    /**
+     * Log console error of `stateChanged`.
+     * Trigger `stateChanged` only when actual state is changed & active property is `true`
+     * When `active` property is not defined it will not prevent `stateChanged` call unless state is not changed.
+     */
+    __stateChanged(state) {
+      try {
+        if (this.__previousState === state || this.active === false) {
+          return;
+        }
+
+        this.stateChanged(state);
+        this.__previousState = state;
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    /**
+     * The `stateChanged(state)` method will be called when the state is updated.
+     */
+    stateChanged(_state) {}
+  };
 //# sourceMappingURL=connect-mixin.js.map
