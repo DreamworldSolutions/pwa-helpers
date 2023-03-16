94adae0d0ba6e1e7b1f3a4cf6a60998d57f6630d
“Nirmal <nirmalb@dreamworld.dev>
2023-03-16 08:41:47 +0530

    fix(layout-mixin): window is not defined on ssr.

--- layout-mixin.js
+++ layout-mixin.js
@@ -1,21 +1,23 @@
+import { isServer } from "./lit.js";
 /**
  * Mixin to apply "mobile" attribute to the component.
  *
  * Intially "mobile" attribute will be set to `true` if viewport width is less than 768px else it will be false.
  * After connectedCallback, it's integrator will determine to update "mobile" property.
  */
-export const layoutMixin = (baseElement) => class extends baseElement {
-  static get properties() {
-    return {
-      /**
-       * Initially it will be set based on
-       */
-      mobile: { type: Boolean, reflect: true }
+export const layoutMixin = (baseElement) =>
+  class extends baseElement {
+    static get properties() {
+      return {
+        /**
+         * Initially it will be set based on
+         */
+        mobile: { type: Boolean, reflect: true },
+      };
     }
-  }
 
-  constructor() {
-    super();
-    this.mobile = window.innerWidth < 768;
-  }
-}
+    constructor() {
+      super();
+      this.mobile = !isServer && window.innerWidth < 768;
+    }
+  };
