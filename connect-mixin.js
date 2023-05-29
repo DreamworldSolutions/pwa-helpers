6e95e4a57a0f152181e4c5c07a6b16e48f7ad4a2
“Nirmal <nirmalb@dreamworld.dev>
2023-05-29 15:41:26 +0530

    fix(connect-mixin): debounce stateChanged from connectedCallback.

--- connect-mixin.js
+++ connect-mixin.js
@@ -18,17 +18,14 @@
 import debounce from 'lodash-es/debounce.js';
 
 export const connect = (store) => (baseElement) => class extends baseElement {
-  constructor() {
-    super();
-    this.__stateChanged = debounce(this.__stateChanged, 50);
-  }
-
   connectedCallback() {
     if (super.connectedCallback) {
         super.connectedCallback();
     }
-    this._storeUnsubscribe = store.subscribe(() => this.__stateChanged(store.getState()));
+
     this.__stateChanged(store.getState());
+    this.__stateChanged = debounce(this.__stateChanged, 50);
+    this._storeUnsubscribe = store.subscribe(() => this.__stateChanged(store.getState()));
   }
   disconnectedCallback() {
       this._storeUnsubscribe && this._storeUnsubscribe();
